e3f32ea321a561eb8f269e790dd2c2201bdf599e
NunesNicolas <nicolas.mgta@gmail.com>
2020-07-23 00:23:05 +0200

    Footer Atoms + Structure

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -1,5 +1,35 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    display:flex;
+    display: flex;
+    align-item: center;
+    max-width: 1400px;
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const Column = styled.div`
+    flex-direction: column;
+    width: 20%;
+`;
+
+export const Link = styled.a`
+    color: #757575;
+    font-size: 1.6rem;
+`;
+
+export const Title = styled.p`
+    
+`;
+
+export const Text = styled.p`
+    
+`;
+
+export const Break = styled.p`
+    
 `;
