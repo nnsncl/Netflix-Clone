100bcd056522c5d4bf6766904f12addc7132cbfe
NunesNicolas <nicolas.mgta@gmail.com>
2020-07-23 22:00:51 +0200

    Create container

---
+++ src/containers/faqs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Accordion } from '../components'
+import FaqsData from '../json/faqs.json'
+
+export function FaqsContainer() {
+    return (
+        <Accordion>
+            <Accordion.Title>Fequently Asked Questions</Accordion.Title>
+            {FaqsData.map((item) => (
+                <Accordion.Item key={item.id}>
+                    <Accordion.Header>{item.header}</Accordion.Header>
+                    <Accordion.Body>{item.body}</Accordion.Body>
+                </Accordion.Item>
+            ))}
+        </Accordion>
+    )
+}
