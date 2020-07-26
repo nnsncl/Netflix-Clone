75fa791077cbfc568ac9b689ef7c626a7ed92005
NunesNicolas <nicolas.mgta@gmail.com>
2020-07-26 23:20:54 +0200

    Add optForm base

---
+++ src/components/opt-form/styles/opt-form.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``;
+export const Input = styled.input``;
+export const Button = styled.button``;
+export const Content = styled.h3`
+    font-size: 1.8rem;
+    padding: 0 3rem;
+`;
