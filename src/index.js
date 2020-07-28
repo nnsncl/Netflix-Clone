21c617a2481db9e2e6981947c56bdd3d482b816c
NunesNicolas <nicolas.mgta@gmail.com>
2020-07-28 23:20:11 +0200

    App config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles'
 import { App } from './App';
+import { FirebaseContext } from './context/firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyARkPY-aArTUHG3b-HSZPNLhIqU4MYdsYU",
+    authDomain: "netflixfeature.firebaseapp.com",
+    databaseURL: "https://netflixfeature.firebaseio.com",
+    projectId: "netflixfeature",
+    storageBucket: "netflixfeature.appspot.com",
+    messagingSenderId: "646375904684",
+    appId: "1:646375904684:web:e957817b46a6427a415609"
+}
+
+const firebase = window.firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
     <>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebase: window.firebase }} >
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
+
     </>,
     document.getElementById('root'));
