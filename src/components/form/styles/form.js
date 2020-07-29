0f9442bdf9da58c1500819184b0c0bd82783f2f1
NunesNicolas <nicolas.mgta@gmail.com>
2020-07-29 23:10:52 +0200

    Component Base

---
+++ src/components/form/styles/form.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { Link as ReachRouterLink } from 'react-router-dom';
+
+export const Frame = styled.form``;
+export const Container = styled.section``;
+export const Input = styled.input``;
+export const Button = styled.button``;
+export const Title = styled.h2``;
+export const Content = styled.p``;
+export const ContentSmall = styled.p``;
+export const Helper = styled.p``;
+export const Link = styled(ReachRouterLink)``;
