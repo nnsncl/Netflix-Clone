04f21c2a73659f2e3727b30ac344aa180807b1c5
NunesNicolas <nicolas.mgta@gmail.com>
2020-07-30 21:53:46 +0200

    Create email/Password states

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,12 +4,34 @@ import { HeaderContainer } from '../containers/header'
 
 export default function Signin() {
     const [error, setError] = useState('');
-    
+    const [emailValue, setEmailValue] = useState('')
+    const [password, setPaswword] = useState('')
+
+    const handleSignIn = (event) => {
+        event.preventDefault()
+        // call in here to firebase to authenticate the user
+        // if there's an error, populate the error state
+    }
+
     return (
         <HeaderContainer>
             <Form>
                 <Form.Title>Sign In</Form.Title>
                 {error && <Form.Helper>{error}</Form.Helper>}
+
+                <Form.Container onSubmit={handleSignIn}  method="POST">
+                    <Form.Input
+                        placeholder='Email adress'
+                        value={emailValue}
+                        onChange={({ target }) => setEmailValue(target.value)}
+                    />
+                    <Form.Input
+                        placeholder='Email adress'
+                        value={password}
+                        onChange={({ target }) => setPaswword(target.value)}
+                    />
+                    <Form.Button></Form.Button>
+                </Form.Container>
             </Form>
         </HeaderContainer>
     )
