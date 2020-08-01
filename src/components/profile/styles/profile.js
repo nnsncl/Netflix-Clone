d56b60ce87a052dfa802ece545745e6ed68bccb7
nnsncl <nicolas.mgta@gmail.com>
2020-08-01 23:48:05 +0200

    Set up profile component

---
+++ src/components/profile/styles/profile.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``;
+export const Title = styled.h1``;
+export const List = styled.ul``;
+export const User = styled.div``;
+export const Picture = styled.img``;
+export const UserName = styled.p``;
