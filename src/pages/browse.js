2195fa0bfee4868d21b237cd706f222b45359f63
nnsncl <nicolas.mgta@gmail.com>
2020-08-06 11:17:20 +0200

    Create custom hook for series and films

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useContent } from '../hooks'
 import { BrowseContainer } from '../containers/browse'
 
 export default function Browse() {
+    const { series } = useContent('series')
+    const { films } = useContent('films')
+
+    console.log(series)
+    console.log(films)
+
     return <BrowseContainer />
 }
