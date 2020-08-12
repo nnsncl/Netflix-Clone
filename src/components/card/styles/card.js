c20db27895d8235dc2b9be519005fae67dbb8669
Nunes <nicolas.mgta+git@gmail.com>
2020-08-12 22:52:13 +0200

    Base card component

---
+++ src/components/card/styles/card.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``;
+
+export const Group = styled.div``;
+
+export const Content = styled.div``;
+
+export const Meta = styled.div``;
+
+export const Title = styled.h5``;
+
+export const Subtitle = styled.h6``;
+
+export const Text = styled.p``;
+
+export const Item = styled.div``;
+
+export const Image = styled.img``;
+
+export const Feature = styled.div``;
+
+export const FeatureTitle = styled.h3``;
+
+export const FeatureText = styled.p``;
+
+export const FeatureClose = styled.img``;
+
+export const Maturity = styled.p``;
