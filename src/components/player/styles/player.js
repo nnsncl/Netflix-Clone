97fe731fb8864d0f37f2dedf3065ab8ed841d068
Nunes <nicolas.mgta+git@gmail.com>
2020-08-26 21:59:01 +0200

    Overlay styles

--- src/components/player/styles/player.js
+++ src/components/player/styles/player.js
@@ -1,9 +1,30 @@
 import styled from 'styled-components'
 
 export const Container = styled.div``;
-export const Overlay = styled.div``;
-export const Inner = styled.div``;
-export const Close = styled.button``;
+
+export const Overlay = styled.div`
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,.95);
+`;
+export const Inner = styled.div`
+    position: relative;
+    width: 100%;
+    max-width: 100%;
+    margin: 0 auto;
+
+    video {
+        height: auto;
+        width: 100%;
+    }
+`;
+
 export const Button = styled.button`
     background: white;
     border: none;
